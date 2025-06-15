c8a9e51defe189d352f00b4eb331d8e326a15900
mikoto2000 <mikoto2000@gmail.com>
2025-06-15 08:23:52 +0000

    rails new したアプリを追加。

---
+++ config/importmap.rb
@@ -0,0 +1,10 @@
+# Pin npm packages by running ./bin/importmap
+
+pin "application"
+pin "@hotwired/turbo-rails", to: "turbo.min.js"
+pin "@hotwired/stimulus", to: "stimulus.min.js"
+pin "@hotwired/stimulus-loading", to: "stimulus-loading.js"
+pin_all_from "app/javascript/controllers", under: "controllers"
+pin "bootstrap", to: "bootstrap.bundle.min.js"
+pin "ya-common", to: "ya-common.js"
+pin "tom-select" # @2.3.0
