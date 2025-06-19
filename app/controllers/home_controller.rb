69f19ea4db245b472afbb63e23f9c9631c790d92
mikoto2000 <mikoto2000@gmail.com>
2025-06-19 14:12:02 +0000

    ユーザー情報が正しくとれなくなっていた問題を修正。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,10 @@
 class HomeController < ApplicationController
 
   def index
-    @pages = []
+    if current_user
+      @pages = Oidc.find_by(issuer: current_user['issuer'], sub: current_user['sub']).account.page
+    else
+      @pages = []
+    end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -13,7 +13,7 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "pages#index"
+  root "home#index"
 
   # ログイン時は、OmniAuth がここを叩く
   get 'auth/:provider/callback', to: 'session#create'
