ac2d97a5ca358f3b1555a6895f9ac0c24f4bcbd1
mikoto2000 <mikoto2000@gmail.com>
2025-06-20 13:52:37 +0000

    謎だが動いた。

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,20 +1,31 @@
 class HomeController < ApplicationController
   def index
-    if current_user
-      @pages = Oidc.find_by(issuer: current_user['issuer'], sub: current_user['sub']).account.page
-    else
-      @pages = []
-    end
-    @new_page = Page.new
+    # アカウント取得
+    account = Oidc.find_by(issuer: current_user['issuer'], sub: current_user['sub']).account
+
+    # bookmarks を View に渡す
+    @bookmarks = account.bookmark
   end
 
   def add_my_bookmark
-    @new_page = Page.create({"url": params[:url]})
-    pages = Oidc.find_by(issuer: current_user['issuer'], sub: current_user['sub']).account.page
-    pages.append @new_page
-    respond_to do |format|
-      format.turbo_stream
-      format.html { redirect_to action: 'index' }
+    # アカウント取得
+    account = Oidc.find_by(issuer: current_user['issuer'], sub: current_user['sub']).account
+
+    # ページの存在確認
+    @new_page = Page.find_by({url: params[:url]})
+
+    # ページが存在していなければ新しいページを作成
+    if @page.nil?
+      @new_page = Page.create({"url": params[:url]})
     end
+
+    # アカウントに紐づくページを追加
+    account_pages = account.page
+    account_pages.append @new_page
+
+    # bookmarks を View に渡す
+    @bookmarks = account.bookmark
+
+    redirect_to action: 'index'
   end
 end
